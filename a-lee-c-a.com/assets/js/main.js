7e38851de117e490eefe28d169eb501f114599f8
aliciasimons <37204250+aliciasimons@users.noreply.github.com>
2019-07-19 14:56:27 +1000

    Add files via upload

---
+++ a-lee-c-a.com/assets/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+
+
+
+  setTimeout(function() {
+  $(".p-sub").addClass("show");
+}, 5000);
+
+setTimeout(function() {
+  $(".p-bye").addClass("show");
+}, 6000);
+
+setTimeout(function() {
+  $(".p-003").addClass("show");
+}, 7000);
+
+setTimeout(function() {
+  $(".p-004").addClass("show");
+}, 8000);
+
+setTimeout(function() {
+  $(".p-005").addClass("show");
+}, 9000);
+
+setTimeout(function() {
+  $(".p-005").addClass("show");
+}, 10000);
+
+setTimeout(function() {
+  $(".p-006").addClass("show");
+}, 11000);
+
+setTimeout(function() {
+  $(".p-007").addClass("show");
+}, 12000);
+
+setTimeout(function() {
+  $(".p-008").addClass("show");
+}, 13000);
+
+$(".pop").on("click", function() {
+  $(this).removeClass("show");
+});
+
+
+
+
+// Add jQuery here
+
+
+
+});
